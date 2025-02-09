fb5871297b615eedecc776f574cb91280c6e3a50
JoelVRV <joelc456789@gmail.com>
2025-02-09 18:28:58 +0100

    FIN TUTORIAL

--- n-react-app/src/App.tsx
+++ n-react-app/src/App.tsx
@@ -4,11 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import  ListGroup from './components/ListGroup';
+import  Alert from './components/Alert';
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* 
+    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+    const handleSelectedItem = (item: string) => {
+      console.log(item);
+    }
+    const [count, setCount] = useState(0)
+    return <div><ListGroup items={items} heading="Cities" onSelectedItem={handleSelectedItem}/></div> 
+  */
 
-  return <div><ListGroup /></div>
+    return (
+        <div>
+          <Alert>
+            Hello World
+          </Alert>
+        </div>
+    );
 }
 
 export default App
--- n-react-app/src/components/ListGroup.tsx
+++ n-react-app/src/components/ListGroup.tsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
 import { Fragment } from "react/jsx-runtime"; // Importamos fragmentos para poder usar varias etiquetas HTML. <> </> Para usar FRAGMENTOS
-import { MouseEvent } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props
+{
+  items: string[];
+  heading: string;
+  // (irem:s tring) => void
+  onSelectedItem: (item: string) => void;
+}
 
-  // Event HANDLER
-  const handleClick = (event: MouseEvent) => console.log(event);
+function ListGroup({items, heading, onSelectedItem}: Props) {
+   // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  heading = "";
+  
 
   // FUNCION MAP: Por cada ITEM dentro de ITEMS, añade el item en un <li>. Para poder añadirlo en el HTML, lo ponemos entre { }
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item active"
+            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
             key={item}
-            onClick={handleClick}
+            onClick={() => {
+                setSelectedIndex(index);
+                onSelectedItem(item);
+              }}
           >
             {item}
           </li>
